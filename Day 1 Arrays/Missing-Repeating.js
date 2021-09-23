1a035e0b7c0550817288ba2cfebee52fa1feb8cc
Jim-Tony <jimtonystarc@gmail.com>
2021-09-23 18:57:02 +0530

    Intial

---
+++ Day 1 Arrays/Missing-Repeating.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return number[]
+ */
+
+class Solution{
+    missRepeat(arr,n){
+        let ans = [];
+        let mp = new Map();
+        //Repeating
+        let one;
+        for(let key of arr){
+            if(mp.has(key) === false) mp.set(key,true);
+            else one = key;  
+        }
+        ans.push(one);
+        //Missing
+        let two;
+        for(let i=1;i<=n;i++){
+            if(mp.has(i) === false) two = i; 
+        }
+        ans.push(two);
+        return ans;
+    }
+};
+let obj  = new Solution();
+let ans = obj.missRepeat([1,3,3],3);
+console.log(ans);
