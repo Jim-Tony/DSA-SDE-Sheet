4475f1fec93e5ee2096f3cb805ba68e7972a2f28
Jim-Tony <jimtonystarc@gmail.com>
2021-10-14 13:29:36 +0530

    Done a dry run

--- Day 5 Linked List/merge_two_sorted_linked_lists.js
+++ Day 5 Linked List/merge_two_sorted_linked_lists.js
@@ -30,3 +30,16 @@
     }
     return res;
 };
+
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+let head1 = new ListNode(1,new ListNode(3,new ListNode(8,new ListNode(9,new ListNode(7)))));
+let head2 = new ListNode(3,new ListNode(4,new ListNode(5,new ListNode(6,new ListNode(10)))));
+var result_head = mergeTwoLists(head1,head2);
+let p = result_head;
+while(p){
+    console.log(p.val);
+    p = p.next;
+}
